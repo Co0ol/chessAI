9c7ca3e97ff9bab6f4f2f4e86fb332083326d5a1
Co0ol <accforlego@gmail.com>
2023-06-17 14:26:37 +0800

    add state value function

--- src/policy/random2.cpp
+++ src/policy/random2.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
-
+#include <set>
+#include<iostream>
 #include "../state/state.hpp"
 #include "./random2.hpp"
 
@@ -11,10 +12,32 @@
  * @param depth You may need this for other policy
  * @return Move 
  */
+
+struct bundle
+{
+  bundle(Move m, int g):move(m), grade(g){}
+  Move move;
+  int grade;
+};
+
+struct cmp
+{
+  bool operator()(const bundle& a, const bundle& b)
+  {
+    return a.grade < b.grade;
+  }
+};
+
+std::set<bundle, cmp> move_priority;
+
 Move Random2::get_move(State *state, int depth){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   
-  auto actions = state->legal_actions;
-  return actions[(rand()+depth)%actions.size()];
+  for(auto action : state->legal_actions)
+  {
+    move_priority.emplace(action, state->next_state(action)->evaluate());
+  }
+  
+  return move_priority.begin()->move;
 }
--- src/state/state.cpp
+++ src/state/state.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdint>
+#include<map>
 
 #include "./state.hpp"
 #include "../config.hpp"
@@ -13,7 +14,69 @@
  */
 int State::evaluate(){
   // [TODO] design your own evaluation function
-  return 0;
+  if (game_state == WIN) return 48763;
+  
+  int our_grade = 0;
+  int our_king = 0;
+  int opponent_grade = 0;
+  int opponent_king = 0;
+  for(int i = 0; i < BOARD_H; i++)
+  {
+    for(int j = 0; j < BOARD_W; j++)
+    {
+      //calculate our grade
+      switch(board.board[player][i][j])
+      {
+        case 1://pawn
+          our_grade += 2;
+          break;
+        case 2://rook
+          our_grade += 6;
+          break;
+        case 3://knight
+          our_grade += 7;
+          break;
+        case 4://bishop
+          our_grade += 8;
+          break;
+        case 5://queen
+          our_grade += 20;
+          break;
+        case 6://king
+          our_king = 1;
+          break;
+        default://case 0
+          continue;
+      }
+      //calculate our grade
+      switch(board.board[1- player][i][j])
+      {
+        case 1://pawn
+          opponent_grade += 2;
+          break;
+        case 2://rook
+          opponent_grade += 6;
+          break;
+        case 3://knight
+          opponent_grade += 7;
+          break;
+        case 4://bishop
+          opponent_grade += 8;
+          break;
+        case 5://queen
+          opponent_grade += 20;
+          break;
+        case 6://king
+          opponent_king = 1;
+          break;
+        default://case 0
+          continue;
+      }
+    }
+  }
+  if(!our_king) return -48763;
+  if(!opponent_king) return 48763;
+  return our_grade - opponent_grade;
 }
 
 
